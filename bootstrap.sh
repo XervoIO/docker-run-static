0cf6fee5fdfe19bc88b11346c103219c1f7a1a24
Taron Foxworth <taronfoxworth@gmail.com>
2015-04-16 23:45:53 -0400

    Fixed some of nginx's errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,16 @@ echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
 sudo apt-get update -y
 sudo apt-get install -y nginx=1.6.3-1+trusty0
-echo "daemon off;" >> /etc/nginx/nginx.conf
+rm /etc/nginx/nginx.conf
+cp /opt/modulus/conf/nginx.conf /etc/nginx/nginx.conf
 rm /etc/nginx/sites-enabled/default
 cp /opt/modulus/conf/nginx-default /etc/nginx/sites-enabled/default
 mkdir /var/lib/nginx/body
+mkdir /var/lib/nginx/proxy
+mkdir /var/lib/nginx/fastcgi
+mkdir /var/lib/nginx/uwsgi
+mkdir /var/lib/nginx/scgi
+touch /var/log/nginx/access.log
+touch /var/log/ngnix/error.log
+chown mop:mop /var/log/nginx/access.log
+chown mop:mop /var/log/nginx/error.log
