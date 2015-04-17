14d8824eb5cf7e7211d9c0b88789515bd3ca1a10
Taron Foxworth <taronfoxworth@gmail.com>
2015-04-16 23:54:09 -0400

    Changing nginx log location ... again

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,6 @@ mkdir /var/lib/nginx/fastcgi
 mkdir /var/lib/nginx/uwsgi
 mkdir /var/lib/nginx/scgi
 mkdir -p /var/log/nginx
-mkdir -p /mnt/log/nginx
 touch /var/log/nginx/access.log
 touch /var/log/nginx/error.log
 chown mop:mop /var/log/nginx/access.log
