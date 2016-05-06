c4c649bfb192b8a40951e48262e60dc670487d7b
Ken Herner <kherner@progress.com>
2016-05-06 13:17:48 -0400

    Update nginx to 1.10.0 as 1.8.1 no longer available

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ export TEMP_DIR=/root/tmp
 echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
 sudo apt-get update -y
-sudo apt-get install -y nginx=1.8.1-1+trusty0
+sudo apt-get install -y nginx=1.10.0-0+trusty0
 rm /etc/nginx/nginx.conf
 cp /opt/modulus/conf/nginx.conf /etc/nginx/nginx.conf
 rm /etc/nginx/sites-enabled/default
